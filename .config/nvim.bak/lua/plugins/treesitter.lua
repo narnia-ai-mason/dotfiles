1cddecbf3eacf17937d835729c68e07b3668bf6c
Mason Seo <mason.seo@narnia.ai>
2025-08-19 02:17:52 +0900

    replace nvim to astronvim and add .tmux

---
+++ .config/nvim.bak/lua/plugins/treesitter.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local config = require("nvim-treesitter.configs")
+      config.setup({
+        auto_install = false,
+        ensure_installed = {
+          "bash",
+          "ruby",
+          "html",
+          "css",
+          "scss",
+          "javascript",
+          "typescript",
+          "json",
+          "lua",
+          "python",
+          "svelte",
+          "tsx",
+          "yaml",
+          "toml",
+          "markdown",
+          "markdown_inline",
+        },
+        highlight = { enable = true },
+        indent = { enable = false },
+      })
+    end
+  }
+}
